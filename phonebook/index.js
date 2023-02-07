aa44e86d06f7e83adf7af24ded98c59f8c4c4e2b
nRibasMercau <nicolas.ribasmercau@gmail.com>
2023-02-08 00:12:47 +0100

    phonebook app front + back

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 morgan.token('person', function getReqBody (request) {
     return JSON.stringify(request.body)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(morgan(':person'))
+app.use(express.static('../front/build'))
 
 let persons = [
     { 
@@ -107,7 +110,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
